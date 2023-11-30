d89869dc3480086aa68d8e3426da901b87a5ea5e
Alejandro N <ale@watsabi.com>
2023-11-30 15:23:42 -0300

    Armado de la estructura sin la lógica

--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from 'react';
+import './Login.css';
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login">
+      <div className="login_container">
+        <div className="column_left" />
+        <div className="column_right">
+          <div className="form_container">
+            <h1>Bienvenido de nuevo</h1>
+            <p>Tus recuerdos esperan</p>
+            <form>
+              <div className="form_group">
+                <label>Correo electrónico</label>
+                <input type="text" placeholder="Correo electrónico" />
+              </div>
+              <div className="form_group">
+                <label>Contraseña</label>
+                <input type="password" placeholder="Contraseña" />
+              </div>
+              <div className="form_group_2col">
+                <div>
+                  <input type="checkbox" />
+                  <label>Recuérdame</label>
+                </div>
+                <div>
+                  <a className="links" href="#">
+                    ¿Olvidaste tu contraseña?
+                  </a>
+                </div>
+              </div>
+              <div className="form_group">
+                <button type="submit">Iniciar sesión</button>
+              </div>
+            </form>
+            <div className="form_group">
+              <p className="nuevo">
+                ¿Eres nuevo?{' '}
+                <a className="links" href="#">
+                  Crea una cuenta
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
--- src/containers/signup/Signup.jsx
+++ src/containers/signup/Signup.jsx
@@ -2,7 +2,52 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className="login">
+  <div className="login_container">
+    <div className="column_left" />
+    <div className="column_right">
+      <div className="form_container">
+        <h1>Crea una cuenta</h1>
+        <p>Proporciona tus datos</p>
+        <form>
+          <div className="form_group">
+            <label>Tu Nombre</label>
+            <input type="text" placeholder="Rogelio Mansilla" />
+          </div>
+          <div className="form_group">
+            <label>Correo electrónico</label>
+            <input type="text" placeholder="ejemplo@gmail.com" />
+          </div>
+          <div className="form_group">
+            <label>Contraseña</label>
+            <input type="password" placeholder="Tdfg%63S" />
+          </div>
+          <div className="form_group_2col">
+            <div>
+              <p className="parrafo">
+                Al registrarme acepto los{" "}
+                <span className="nuevo">términos y condiciones</span> y{" "}
+                <span className="nuevo">política de privacidad</span>
+              </p>
+            </div>
+          </div>
+          <div className="form_group">
+            <button type="submit">Iniciar sesión</button>
+          </div>
+        </form>
+        <div className="form_group">
+          <p className="nuevo">
+            ¿Ya Eres usuario?{" "}
+            <a className="links" href="#">
+              Inicia Sesión
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
   )
 }
 
