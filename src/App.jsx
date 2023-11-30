0d1cb5f211968b21955b5161878cedc90f245fc9
Cristian Contreras <contreras.cristian994@gmail.com>
2023-11-30 13:41:11 -0500

    update home page and header

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,24 @@ import HomePage from './containers/homePage/HomePage';
 import Login from './containers/login/Login';
 import Signup from './containers/signup/Signup';
 import ProfilePage from './containers/profilePage/ProfilePage';
+import Recordatorios from './containers/Recordatorios/Recordatorios';
+import Header from './components/header/Header';
+import Galeria from './containers/galeria/Galeria';
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/login" element={<Login /> } />
-    <Route path="/signup" element={<Signup /> } />
-    <Route path="/profile" element={<ProfilePage /> } />
-  </>
-));
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  const router = createBrowserRouter(createRoutesFromElements(
+    <>
+      <Route path="/" element={<><Header /> <HomePage /></>} />
+      <Route path="/login" element={<Login /> } />
+      <Route path="/signup" element={<Signup /> } />
+      <Route path="/profile" element={<ProfilePage /> } />
+      <Route path="/reminders" element={<Recordatorios /> } />
+      <Route path="/gallery" element={<Galeria /> } />
+    </>
+  ));
 
   return (
     <RouterProvider router={router} />
