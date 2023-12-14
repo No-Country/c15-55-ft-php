66c9f8403f366e8378bbf17abe9791428e7003fb
Cristian Contreras <contreras.cristian994@gmail.com>
2023-12-14 14:50:59 -0500

    added add/delete reminder

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,9 @@ import LandingPage from './containers/landingPage/LandingPage';
 import users from './data/users';
 import fotosdata from './data/fotosdata';
 import recordatoriosdata from './data/recordatoriosdata';
+import AddReminder from './components/reminderForm/AddReminder';
 
+import { db } from "../src/config/firestore";
 import { collection, getDocs } from "firebase/firestore";
 
 
@@ -26,14 +28,32 @@ function App() {
   const [fotos, setFotos] = useState(fotosdata);
   const [recordatorios, setRecordatorios] = useState(recordatoriosdata);
   // abrir console en developer tools para ver nuestra data:
-  console.log(usersData, fotos, recordatorios);
+  // console.log(usersData, fotos, recordatorios);
+
+  const [reminders, setReminders] = useState();
+
+  const getReminders = async() => {
+    const querySnapshot = await getDocs(collection(db, "reminders"));
+    const reminders = querySnapshot.docs.map(doc => ({id: doc.id, ...doc.data()}));
+    // querySnapshot.forEach((doc) => {
+    //   console.log(`${doc.id} => ${doc.data()}`);
+    //   console.log(doc.id, " => ", doc.data());
+    // });  
+    setReminders(reminders);
+  };
+
+  useEffect(() => {
+    getReminders();
+    console.log(reminders);
+  }, [setReminders]);
 
   const router = createBrowserRouter(createRoutesFromElements(
     <>
-      <Route path="landing" element={<LandingPage /> } />
+      <Route path="/landing" element={<LandingPage /> } />
       <Route path="/" element={<Root />} >
         <Route path="homePage" element={<HomePage />} />
-        <Route path="reminders" element={<Recordatorios /> } />
+        <Route path="reminders" element={<Recordatorios reminders={reminders} setReminders={setReminders} getReminders={getReminders} /> } />
+        <Route path="add" element={<AddReminder getReminders={getReminders}/>} />
         <Route path="gallery" element={<Galeria /> } />    
         <Route path="/profile" element={<ProfilePage /> } />
       </Route>
--- src/components/recuerdosCard/recuerdoCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function recuerdoCard() {
-  return (
-    <div>recuerdoCard</div>
-  )
-}
-
-export default recuerdoCard;
